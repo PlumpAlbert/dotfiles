d9f33159e5966137c1effce87ab61504bfbbc5e9
plump_albert <plumpalbert@gmail.com>
2020-08-22 11:53:45 +0300

    Updated jack_init script

--- .local/scripts/jack_init.sh
+++ .local/scripts/jack_init.sh
@@ -11,24 +11,13 @@ jack_control dps rate 96000
 jack_control dps nperiods 2
 jack_control dps period 768
 
+# Kill any running PulseAudio daemon
+pulseaudio -k
+# Start PulseAudio
+pulseaudio -D -F "$HOME/.config/pulse/config.pa"
+
 # Unload pulseaudio `module-jackdbus-detect`
-module="$(pactl list modules | grep -B 1 -i 'module-jackdbus-detect' | grep -o '[[:digit:]]\+')"
-pactl unload-module $module
-
-# Unload pulseaudio `module-jack-sink`
-module_sink="$(pactl list modules | grep -B 1 -i 'module-jack-sink' | grep -o '[[:digit:]]\+')"
-[ -z $module_sink ] && pactl unload-module $module_sink
-# Default sink for applications
-module_index=$(jsink SystemSounds)
-pactl set-default-sink SystemSounds
-# Sink for Discord
-module_index=$(jsink Discord)
-
-# Unload pulseaudio `module-jack-source`
-module_source="$(pactl list modules | grep -B 1 -i 'module-jack-source' | grep -o '[[:digit:]]\+')"
-[ -z $module_source ] && pactl unload-module $module_source
-module_index=$(jsource Microphone)
-pactl set-default-source Microphone
+pactl unload-module "$(pactl list short modules | grep -i 'module-jackdbus-detect' | cut -f1)"
 
 # Connecting clients
 ## Monitoring microphone
