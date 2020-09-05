b696be82c122b03b5d678cbe4c01a81db6bebe99
plump_albert <plumpalbert@gmail.com>
2020-09-05 13:39:48 +0300

    Updated jack_init script

--- .local/scripts/jack_init.sh
+++ .local/scripts/jack_init.sh
@@ -6,18 +6,19 @@ jack_control stop
 # Audio settings
 jack_control start
 jack_control ds alsa
-jack_control dps device hw:USB
-jack_control dps rate 96000
+jack_control dps device 'hw:USB,0'
+jack_control dps rate 48000
 jack_control dps nperiods 2
-jack_control dps period 768
+jack_control dps period 512
 
 # Kill any running PulseAudio daemon
 pulseaudio -k
+sleep 1s
 # Start PulseAudio
 pulseaudio -D -F "$HOME/.config/pulse/config.pa"
-
 # Unload pulseaudio `module-jackdbus-detect`
 pactl unload-module "$(pactl list short modules | grep -i 'module-jackdbus-detect' | cut -f1)"
+
 
 # Connecting clients
 ## Monitoring microphone
