5446e4242e280b9064d8d400d351067043d229d3
plump_albert <plumpalbert@gmail.com>
2020-10-19 16:47:18 +0300

    Updated script for rescuetime block

--- .config/polybar/scripts/rt.sh
+++ .config/polybar/scripts/rt.sh
@@ -3,13 +3,13 @@
 ICON="👨💻"
 ! [ -f "$HOME/.local/share/rescuetime.key" ] && echo $ICON
 key="$(cat "$HOME/.local/share/rescuetime.key")"
-url="https://www.rescuetime.com/anapi/data\
-  ?key=$key\
-  &format=csv\
-  &i=day\
-  &rb=$(date '+%F')\
-  &re=$(date '+%F')\
-  &rk=productivity"
+url="https://www.rescuetime.com/anapi/data?\
+key=$key\
+&format=csv\
+&i=day\
+&rb=$(date '+%F')\
+&re=$(date '+%F')\
+&rk=productivity"
 seconds="$(curl -sL $url | awk -F, '$4 > 0 { sum += $2 } END {print sum}')"
 
-echo "$ICON $(date -u -d @${seconds} +'%H:%M:%S')"
+echo "$ICON $(date -u -d "@$seconds" +'%H:%M:%S')"
